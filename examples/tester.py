b7e89be56b994b52c39c94f762bea1ef34fe24fa
tginart <tginart@stanford.edu>
2018-06-16 11:22:09 -0700

    demo cleaned -- almost done

--- examples/tester.py
+++ examples/tester.py
@@ -2,6 +2,13 @@ import tests
 import smallfry as sfry
 import numpy as np
 
+
+# Generate a random uniform matrix
 unimat = 2*np.random.random(1000,50)-1
+
+np.savetxt("uni.mat",unimat,fmt='%.12f')
+
+
+
 
 
