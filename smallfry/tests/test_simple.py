50f4b59e08e6dac720cc44b3252092370ad1abad
tginart <tginart@stanford.edu>
2018-06-16 15:16:41 -0700

    __

--- smallfry/tests/test_simple.py
+++ smallfry/tests/test_simple.py
@@ -1,9 +1,28 @@
-import smallfry
+import smallfry as sfry
+import tests
+import numpy as np
+import os
 import pytest
 
-def test_bad():
-    a = 1
-    b = 1
+def gen_uniform_embs():
+    nums = np.array([i for i in range(0,1000)])
+    uni_test_prior = dict()
+    for i in range(0,1000):
+        uni_test_prior[str(i)] = 10*np.random.random()
+    np.savetxt("wordnums",nums,fmt='%i')
+    np.save("uni_test_prior",uni_test_prior)
+    unimat = 2*np.random.random([1000,50])-1
+    np.savetxt("uni.mat",unimat,fmt='%.12f')
+    os.system("paste -d ' ' uni.mat wordnums > uniembs.txt")
+    word2idx, sfry_path = sfry.compress("uniembs.txt", "uni_test_prior.npy", write_inflated=True, word_rep="dict")
+    return word2idx, sfry_path
+
+
+def test_inflation_fidelity():
+    word2dix, sfry_path = gen_uniform_embs()
+    
+    
+     
     assert(a == b)
 
 def test_good():
