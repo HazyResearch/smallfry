14cf15e64d7abb1215cc30d4bdf850e5a93d6ca2
tginart <tginart@stanford.edu>
2018-06-23 02:34:53 -0700

    test updated

--- smallfry/tests/test_simple.py
+++ smallfry/tests/test_simple.py
@@ -113,14 +113,14 @@ def gen_uniform_embs(uniprior=False):
     unimat = 2*np.random.random([1000,100])-1
     np.savetxt("uni.mat",unimat,fmt='%.12f')
     os.system("paste -d ' '  wordnums uni.mat > uniembs.txt")
-    word2idx, sfry_path = sfry.compress("uniembs.txt", "uni_test_prior.npy", write_inflated=True, word_rep="dict")
+    word2idx, sfry_path = sfry.compress("uniembs.txt", "uni_test_prior.npy", "out" , write_inflated=True, word_rep="dict")
     return word2idx, sfry_path
 
 
 def test_inflation_fidelity():
     word2idx, sfry_path = gen_uniform_embs()
     #TODO fix inflated naming!!!
-    infpath = "uniembs.txt.sfry.inflated_R_1_seed_None_max_bitrate_None_batch_full_sampling_True"
+    infpath = sfry_path+"/sfry.inflated"
     check_mmap = check_inflation(infpath, sfry_path, word2idx, mmap=True)
     #check_file = check_inflation(infpath, sfry_path, word2idx, mmap=False)
    
@@ -157,7 +157,7 @@ def test_approx_codebk_correctness():
 
 def test_weighted_fronorm():
     word2idx, sfry_path = gen_uniform_embs(uniprior=True)
-    infpath = "uniembs.txt.sfry.inflated_R_1_seed_None_max_bitrate_None_batch_full_sampling_True" 
+    infpath = sfry_path+"/sfry.inflated" 
     #we can solve this problem explicitly to get ~0.041 as the per entry distortion
     net_dist = 0.0833*100*1000
     dist = weighted_fronorm("uniembs.txt",infpath,"uni_test_prior.npy")
