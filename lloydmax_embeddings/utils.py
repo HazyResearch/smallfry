d77fbe196b8bb876e5d93e82c1e8b0a919e2d88b
tginart <tginart@stanford.edu>
2018-08-23 11:04:15 -0700

    is the package fixed?

--- lloydmax_embeddings/utils.py
+++ lloydmax_embeddings/utils.py
@@ -1,13 +1,31 @@
 import numpy as np
+import json
+import bitarray as ba
 
-def serialize(bit_arr, metadata, filepath)
+def serialize(bit_arr, metadata, out_path)
     '''
     Writes a lloyd-max quantized binary and metadata to a directory
     '''
-   lmqbin_filepath = filepath + '.lmqbin'
+    lmqbin_filepath = out_path + '.lmqbin'
+    metadata_filepath = out_path + '.meta'
 
+    lmqbin_file = open(lmqbin_filepath,'wb')
+    metadata_file = open(metadata_filepath,'w')
+
+    bit_arr.tofile(lmqbin_file)
+    metadata_file.write(json.dumps(metadata))
 
 def deserialize(lmqbin_filepath, metadata_filepath):
     '''
     Loads a lloyd-max quantized binary and the metadata from file
     '''
+
+    lmqbin_file = open(lmqbin_filepath,'rb')
+    metadata_file = open(metadata_filepath,'r')
+
+    bit_arr = ba.bitarray()
+    bit_arr.fromfile(lmqbin_file)
+
+    return bit_arr, json.loads(metadata_file.read())
+
+
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
-setuptools.setup(name='smallfry',
+setuptools.setup(name='lmqe',
       version='0.1',
-      description='Insanely compressed word embeddings',
+      description='Lloyd-Max quantizer for embeddings',
       url='https://github.com/HazyResearch/small-fry',
       author='tginart et al.',
       author_email='tginart@stanford.edu',
