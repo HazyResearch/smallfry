cbbe34120fdf7876cf30c5cc2cae30051869fa10
tginart <tginart@stanford.edu>
2018-08-27 00:12:19 -0700

    added tests?

---
+++ lloydmax_embeddings/tests/test_simple.py
@@ -0,0 +1,42 @@
+import numpy as np
+import argh
+import marisa_trie
+import os
+import lloydmax_embeddings as lmqe
+import torch
+
+
+demo_embed_path = '../../examples/data/glove.head.txt'
+
+def torch_embed():
+    X = np.random.random([100,10])
+    lmq_bin, meta = lmqe.core.quantize(X)
+    e = lmqe.Embedding.LloydMaxEmbedding(lmq_bin, meta)
+    idx = torch.IntTensor([[4,1,2],[3,4,5]])
+    print(e(idx))
+
+    assert True
+
+
+def io():
+    vocab, embeddings = lmqe.utils.load_embeddings(demo_embed_path)
+    lmq_bin, metadata, wordtrie = lmqe.utils.compress(embeddings, vocab)
+    lmqe.utils.serialize(lmq_bin, metadata, 'meep')
+
+    assert True
+
+def compression():
+    vocab, embeddings = lmqe.utils.load_embeddings(demo_embed_path)
+    lmqe.utils.compress(embeddings, vocab)
+    assert True
+
+
+#def kmeans():
+
+
+parser = argh.ArghParser()
+parser.add_commands([torch_embed, io, compression])
+
+if __name__ == '__main__':
+    parser.dispatch()
+
