ee9af36d5a9650cf0c365d1f427a57918a0c8bf1
Avner May <avnermay@gmail.com>
2018-09-19 21:02:39 +0000

    mcc dca hyper sweep added

--- experiments/maker/maker_cmd_creator.py
+++ experiments/maker/maker_cmd_creator.py
@@ -68,6 +68,24 @@ def sweep(method, rungroup, base_embeds, base_embeds_path, seeds, params, qsub=T
 '''
 LAUNCH ROUTINES BELOW THIS LINE =========================
 '''
+def launch_experiment1_dca_tune_400K(name):
+    #date of code Sept 19, 2018
+    rungroup = 'experiment1-dca-tune'
+    methods = ['dca']
+    bitrates = [0.1,0.25,0.5,1,2,4]
+    global qsub_log_path
+    qsub_log_path = maker.prep_qsub_log_dir(qsub_log_path, name, rungroup)
+    params = dict()
+    for method in methods:
+        base_embeds = ['fasttext','glove']
+        base_path_ft = str(pathlib.PurePath(maker.get_base_embed_path_head(), 'fasttext_k=400000'))
+        base_path_glove = str(pathlib.PurePath(maker.get_base_embed_path_head(), 'glove_k=400000'))
+        base_embeds_path = [base_path_ft, base_path_glove]
+        seeds = [int(np.random.random()*10000), int(np.random.random()*10000), int(np.random.random()*10000)]
+        #line below assumes all base embeddings are of the same size!
+        method_params = dca_hyperparam_sweep(bitrates, base_embeds_path[0])
+        sweep(method, rungroup, base_embeds, base_embeds_path, seeds, method_params)
+    log_launch(maker.get_log_name(name, rungroup))
 
 def launch_testrun4(name):
     #date of code Sept 18, 2018
@@ -82,11 +100,10 @@ def launch_testrun4(name):
         base_embeds = ['fasttext']
         base_path = str(pathlib.PurePath(maker.get_base_embed_path_head(), 'fasttext_k=400000'))
         base_embeds_path = [base_path]
-        seeds = [20]
+        seeds = [int(np.random.random()*1000i), int(np.random.random()*1000), int(np.random.random()*1000)]
         method_params = params[method]
         sweep(method, rungroup, base_embeds, base_embeds_path, seeds, method_params)
     log_launch(maker.get_log_name(name, rungroup))
-
 
 def launch_debug_githash(name):
     #date of code Sept 18, 2018
