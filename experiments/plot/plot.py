06ececb1f0a8a9218d702ea6133a2c2aaec8f62b
Avner May <avnermay@gmail.com>
2018-09-23 06:00:27 +0000

    plots stuff is good

--- experiments/plot/plot.py
+++ experiments/plot/plot.py
@@ -105,25 +105,29 @@ def compute_avg(data):
         data_y.append(data_d[data_x[i]])
     return data_x, data_y
 
-def get_dca_params(dca_hp_tune_rg, bitrates, base):
-    res = agg(get_base_outputdir(), dca_hp_tune_rg+'/*'+base+'*')
+def get_dca_params(results, bitrates, base):
+    res = results
     br_2_mks = dict()
     for br in bitrates:
+        br_2_mks[br] = []
         for r in res:
+            if r == {} or r['base'] != base: continue
             if r['method'] == 'dca' and abs(r['bitrate'] - br) < 0.15*br:
-                br_2_mks = (r['m'],r['k'],r['mean-euclidean-dist'])
+                br_2_mks[br].append((r['m'],r['k'],r['embed-fro-dist']))
+                br_2_mks[br].sort(key=lambda x:x[1])
     return br_2_mks
 
-def get_dca_best_params(dca_hp_tune_rg, bitrates, base):
-    res = agg(get_base_outputdir(),dca_hp_tune_rg+'/*'+base+'*')
+def get_dca_best_params(results, bitrates, base):
+    res = results
     br_2_mk = dict()
     for br in bitrates:
         lowest_mdd = 9999999
         best_res = None
         for r in res:
+            if r == {} or r['base'] != base: continue
             if r['method'] == 'dca' and abs(r['bitrate'] - br) < 0.15*br:
-                if lowest_mdd > r['mean-euclidean-dist']:
-                    lowest_mdd = r['mean-euclidean-dist']
+                if lowest_mdd > r['embed-fro-dist']:
+                    lowest_mdd = r['embed-fro-dist']
                     best_res = r
         br_2_mk[br] = (best_res['m'], best_res['k'])
-    return br_2_m
+    return br_2_mk
