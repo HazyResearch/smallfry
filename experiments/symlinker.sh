bf07594ac124e231a20d2d08a58805f8aad43488
Tony Ginart <tginart@stanford.edu>
2018-09-25 19:58:43 +0000

    fixed up symlinker

--- experiments/symlinker.sh
+++ experiments/symlinker.sh
@@ -1,12 +1,11 @@
 #THIS IS A SIMPLE COMMAND LINE UTILITY SCRIPT THAT HELPS MAKE SYMBOLIC LINKS FOR RUNGROUPS THAT ARE SPREAD ACROSS TIME AND SPACE
-rg_name=$0
-date1=$1
-date2=$2
-for p in $(ls -d /proj/smallfry/embeddings/$1-$0 && ls -d /proj/smallfry/embeddings/$2-$0); do
-    joinDir=$($(dirname $p)/merged-$0)
+rg_name=$1
+date1=$2
+date2=$3
+for p in $(ls -d /proj/smallfry/embeddings/$2-$1 && ls -d /proj/smallfry/embeddings/$3-$1); do
+    joinDir=$(dirname $p)/merged-$1
     mkdir -p ${joinDir}
     for oldRunDir in $(ls -d $p/*); do
-        unlink $joinDir/$(filename $oldRunDir)
         ln -s -f $oldRunDir $joinDir
     done
 done
