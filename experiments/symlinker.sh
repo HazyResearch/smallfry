d9c6831dd9c04d39e9da219591ef4420aa15c63c
tginart <tginart@stanford.edu>
2018-09-25 14:04:23 -0700

    symlinker now merges all dates into one rungroup

--- experiments/symlinker.sh
+++ experiments/symlinker.sh
@@ -1,8 +1,6 @@
 #THIS IS A SIMPLE COMMAND LINE UTILITY SCRIPT THAT HELPS MAKE SYMBOLIC LINKS FOR RUNGROUPS THAT ARE SPREAD ACROSS TIME AND SPACE
 rg_name=$1
-date1=$2
-date2=$3
-for p in $(ls -d /proj/smallfry/embeddings/$2-$1 && ls -d /proj/smallfry/embeddings/$3-$1); do
+for p in $(ls -d /proj/smallfry/embeddings/*-$1); do
     joinDir=$(dirname $p)/merged-$1
     mkdir -p ${joinDir}
     for oldRunDir in $(ls -d $p/*); do
