18e3d748705ff562ea1705de73e98c8d58162566
Tony Ginart <tginart@stanford.edu>
2018-09-28 17:33:13 +0000

    plot stuff

--- experiments/plot/plotter.py
+++ experiments/plot/plotter.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import argh
+import numpy as np
 import matplotlib.pyplot as plt
 from plot_tools import *
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..')) #FOR LOCAL IMPORTS
@@ -60,12 +61,23 @@ def plot_embeddings_frobenius(qry='merged-experiment2-5X-seeds/*',seeds=[4974,61
                     #print(errbars_high)
                     color = 'r' if method == 'dca' else 'b'
                     plt.errorbar(data_x, data_y, fmt=color, yerr=errbars, linewidth=3.0, label=method)
-                #plt.axhline(y=np.mean(baselines),linestyle='--',label='baseline (32-bit)',linewidth=3.0)
-                data = get_all_data(agg(qry,expected_num_res=130), source, vocab, 'baseline', x[i], y[i])
-                data_x,data_y = compute_avg_variable_len(data)
-                plt.axhline(y=data_y,linestyle='--',label='baseline (32-bit)',linewidth=3.0)
+                #plt.axhline(y=np.mean(baselines),linestyle='--',label='baseline (32-bit)',linewidth=3.0)i
+                if y[i] in ['max-f1', 'analogy-avg-score', 'similarity-avg-score']:
+                    data = get_all_data(agg(qry,expected_num_res=130), source, vocab, 'baseline', x[i], y[i])
+                    print(data)
+                    vals = data[32.0]
+
+                    data_x = [0.1,0.25,0.5,1,2,4]
+                    data_y = [np.mean(np.array(vals))]*6
+                    errbar = 0.5*(max(vals) - min(vals))
+                    print(data_x)
+                    print(data_y)
+                    plt.errorbar(data_x, data_y, fmt='c', yerr=errbar, label='baseline (32-bit)', linewidth=3.0, linestyle='--')
+                #plt.axhline(y=data_y,linestyle='--',label='baseline (32-bit)',linewidth=3.0)
                 plt.xlabel(nice_names_lookup(x[i]), size=lbl_size)
                 plt.ylabel(nice_names_lookup(y[i]), size=lbl_size)
+                plt.xscale('log')
+                plt.xticks([0.1,0.25,0.5,1,2,4],['0.1','0.25','0.5','1','2','4'])
                 plt.title('%s vs. %s for %s' % (nice_names_lookup(x[i]),
                                                     nice_names_lookup(y[i]),
                                                     nice_names_lookup(source)))
