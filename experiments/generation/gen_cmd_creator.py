137742e69215875a191da365e54cddc34f069b26
tginart <tginart@dawn17.stanford.edu>
2018-10-09 22:04:07 -0700

    more in gen_cmd_creator

--- experiments/generation/gen_cmd_creator.py
+++ experiments/generation/gen_cmd_creator.py
@@ -17,7 +17,7 @@ qsub_preamble = "qsub -V -b y -wd"
 def launch_config(config, qsub=False):
     s = ''
     global qsub_preamble
-    maker_path = str(pathlib.PurePath(os.path.dirname(os.path.realpath(__file__)),'maker.py'))
+    maker_path = str(pathlib.PurePath(os.path.dirname(os.path.realpath(__file__)),'generate.py'))
     python_maker_cmd = 'python %s' % maker_path
     flags = [python_maker_cmd]
     for key in config.keys():
@@ -47,6 +47,27 @@ def sweep_configs(configs):
 LAUNCH ROUTINES BELOW THIS LINE =========================
 '''
 
+def generate_dim_reduction2_exp6_10_9_18(name):
+    rungroup = 'experiment6-dim-reduc-mini'
+    method = 'glove'
+    ibrs = [6, 32]
+    global qsub_log_path
+    qsub_log_path = generate.prep_qsub_log_dir(qsub_log_path, name, rungroup)
+    configs = []
+    for ibr in ibrs:
+        config = dict()
+        config['rungroup'] = rungroup
+        config['method'] = method
+        config['corpus'] = 'text8'
+        config['dim'] = ibr_2_dim(ibr)
+        config['outputdir'] = generate.get_base_outputdir()
+        config['seed'] = 1234
+        configs.append(config)
+    sweep_configs(configs)
+    log_launch(generate.get_log_name(name, rungroup))
+
+
+
 def generate_dim_reduction_exp6_10_9_18(name):
     rungroup = 'experiment6-dim-reduc-mini'
     method = 'glove'
@@ -67,7 +88,7 @@ def generate_dim_reduction_exp6_10_9_18(name):
     log_launch(generate.get_log_name(name, rungroup))
 
 #IMPORTANT!! this line determines which cmd will be run
-cmd = [generate_dim_reduction_exp6_10_9_18]
+cmd = [generate_dim_reduction2_exp6_10_9_18]
 
 parser = argh.ArghParser()
 parser.add_commands(cmd)
