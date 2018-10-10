9030a5c30d9a2ccd7adc4ff891e032ce6b358dd0
tginart <tginart@stanford.edu>
2018-10-09 22:59:59 -0700

    added launch config to mcc

--- experiments/maker/maker_cmd_creator.py
+++ experiments/maker/maker_cmd_creator.py
@@ -54,6 +54,17 @@ def qsub_launch_config(config):
         raise ValueError(f"bad method name in launch: {config['method']}")
     return s
 
+def launch_config(config, qsub=False):
+    s = ''
+    global qsub_preamble
+    maker_path = str(pathlib.PurePath(os.path.dirname(os.path.realpath(__file__)),'generate.py'))
+    python_maker_cmd = 'python %s' % maker_path
+    flags = [python_maker_cmd]
+    for key in config.keys():
+        flags.append(f"--{key} {config[key]}")
+    s = " ".join(flags)
+    s = f"{qsub_preamble} {qsub_log_path} {s}" if qsub else s
+    return s
 
 '''
 HELPER METHODS FOR COMMON SWEEP STYLES (and logging)
