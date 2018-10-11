af6ec77e9078a2d23134e80fb6d828dd51cbac55
tginart <tginart@stanford.edu>
2018-10-11 16:27:24 -0700

    X clip an X quant numpy data torch

--- experiments/maker/uniform_quant.py
+++ experiments/maker/uniform_quant.py
@@ -62,7 +62,8 @@ def optranuni(X,br,eps=1e-40,tol=0.1,L_max=10):
     quant = lambda X,L: uniquant(X,br,L) #bitrate does not change, no reason to pass it in each time
     f = lambda X,X_q: _compute_frobenius(X,X_q)
     L_star = _goldensearch(X,f,quant,eps=eps,tol=tol,L_max=L_max)
-    return quant(X,L_star)
+    X_q = quant(X,L_star)
+    return X_q.data.numpy()
 
 def clipnoquant(X,br):
     br = int(br)
