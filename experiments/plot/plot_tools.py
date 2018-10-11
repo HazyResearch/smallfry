c85aafed00c5b83279e2bf85df7348b962a3c0d2
tginart <tginart@dawn17.stanford.edu>
2018-10-11 15:26:44 -0700

    committing

--- experiments/plot/plot_tools.py
+++ experiments/plot/plot_tools.py
@@ -261,15 +261,15 @@ def make_plots( x,
         print(y)
         print(source)
         vals = data[32.0]
-        data_x = [0.1,0.25,0.5,1,2,4,6]
-        data_y = [np.mean(np.array(vals))]*7
+        data_x = [0.1,0.25,0.5,1,2,4]
+        data_y = [np.mean(np.array(vals))]*6
         errbar = 0.5*(max(vals) - min(vals)) #TODO fix this weird error bar centering
         plt.errorbar(data_x, data_y, fmt=color_lookup('baseline'), yerr=errbar, label='baseline (32-bit)', linewidth=3.0, linestyle='--')
     plt.xlabel(nice_names_lookup(x), size=lbl_size)
     plt.ylabel(nice_names_lookup(y), size=lbl_size)
     plt.xscale(xscale)
     plt.yscale(yscale)
-    plt.xticks([0.1,0.25,0.5,1,2,4,6],['0.1','0.25','0.5','1','2','4','6'])
+    plt.xticks([0.1,0.25,0.5,1,2,4],['0.1','0.25','0.5','1','2','4'])
     plt.title('%s vs. %s for %s' % (nice_names_lookup(x),
                                     nice_names_lookup(y),
                                     nice_names_lookup(source)))
