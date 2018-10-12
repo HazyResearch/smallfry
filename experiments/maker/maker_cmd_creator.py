4d355fb8cd46eeacb3f59dd7157226f09778934f
tginart <tginart@dawn17.stanford.edu>
2018-10-11 19:23:27 -0700

    command ready

--- experiments/maker/maker_cmd_creator.py
+++ experiments/maker/maker_cmd_creator.py
@@ -1010,6 +1010,7 @@ def test_kmeans_10_11_18(name):
     rungroup = 'test-clipnoquant-and-goldensearch'
     methods = ['kmeans']
     ibrs = [1,2,4]
+    bpb = [1,2,4]
     global qsub_log_path
     qsub_log_path = maker.prep_qsub_log_dir(qsub_log_path, name, rungroup)
     configs = []
@@ -1024,13 +1025,40 @@ def test_kmeans_10_11_18(name):
             config['method'] = method
             config['outputdir'] = maker.get_base_outputdir()
             config['seed'] = 1234
+            config['bitsperblock'] = bpb[i]
+            config['blocklen'] = 1
             configs.append(config)
     sweep_configs(configs)
     log_launch(maker.get_log_name(name, rungroup))
 
+def make_quant_ablation_exp7_10_11_18(name):
+    rungroup = 'experiment7-quant-ablation'
+    methods = ['optranuni','clipnoquant','kmeans']
+    ibrs = [0.5,1,2,4,8]
+    global qsub_log_path
+    qsub_log_path = maker.prep_qsub_log_dir(qsub_log_path, name, rungroup)
+    configs = []
+    for method in methods:
+        for i in range(len(ibrs)):
+            config = dict()
+            if ibrs[i] < 0.999 and method != 'kmeans':
+                continue
+            config['ibr'] = ibrs[i]
+            config['rungroup'] = rungroup
+            config['base'] = 'glove'
+            config['basepath'] = str(pathlib.PurePath(maker.get_base_embed_path_head(),
+             'corpus=text8,method=glove,maxvocab=100000,dim=300,memusage=128,seed=1234,date=2018-10-09,rungroup=experiment6-dim-reduc-mini.txt'))
+            config['method'] = method
+            config['outputdir'] = maker.get_base_outputdir()
+            config['seed'] = 1234
+            config['bitsperblock'] = ibrs[i]
+            config['blocklen'] = 1
+            configs.append(config)
+    sweep_configs(configs)
+    log_launch(maker.get_log_name(name, rungroup))
 
 #IMPORTANT!! this line determines which cmd will be run
-cmd = [test_kmeans_10_11_18]
+cmd = [make_quant_ablation_exp7_10_11_18]
 
 parser = argh.ArghParser()
 parser.add_commands(cmd)
