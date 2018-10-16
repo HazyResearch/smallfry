f721d845d183c973bf089e9a037331be3bcecbfa
tginart <tginart@stanford.edu>
2018-10-16 16:42:30 -0700

    uni quant stochoptranuni updated

--- experiments/maker/uniform_quant.py
+++ experiments/maker/uniform_quant.py
@@ -78,8 +78,7 @@ def stochoptranuni(X,br,seed=1234,eps=1e-40,tol=0.1,L_max=10):
     quant = lambda X,L: uniquant(X,br,L) #bitrate does not change, no reason to pass it in each time
     f = lambda X,X_q: _compute_frobenius(X,X_q)
     L_star = _goldensearch(X,f,quant,eps=eps,tol=tol,L_max=L_max)
-    X_c = torch.clamp(torch.Tensor(X), min=-1*L_star, max=L_star)
-    X_q = stochround(X_c,L_star,seed)
+    X_q = clamp_and_quantize(X, br, range_limit=L_star, stochastic_round=True)
     return X_q.numpy()
 
 def clipnoquant(X,br):
