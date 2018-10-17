b4dae09e10349f6a0cd473f912e05436f6e2d8f5
tginart <tginart@stanford.edu>
2018-10-17 10:03:19 -0700

    ecc ready for launch

--- experiments/evaluation/eval_cmd_creator.py
+++ experiments/evaluation/eval_cmd_creator.py
@@ -72,8 +72,23 @@ def launch_eval_exp9_10_16_18(name):
     sweep_configs(configs, False)
     log_launch(evaluate.get_log_name(name,rungroup),batchsize=3)
 
+def launch_eval_exp8_10_17_18(name):
+    rungroup = 'merged-exp8-wiki-trained'
+    evaltypes = ['QA','intrinsics']
+    embs = evaluate.get_all_embs_in_rg(rungroup)
+    configs = []
+    for evaltype in evaltypes:
+        for emb in embs:
+            config = dict()
+            config['evaltype'] = evaltype
+            config['embedpath'] = emb
+            config['seed'] = 1234
+            configs.append(config)
+    sweep_configs(configs, False)
+    log_launch(evaluate.get_log_name(name,rungroup),batchsize=3)
+
 #IMPORTANT!! this line determines which cmd will be run
-cmd = [launch_eval_exp9_10_16_18]
+cmd = [launch_eval_exp8_10_17_18]
 
 parser = argh.ArghParser()
 parser.add_commands(cmd)
