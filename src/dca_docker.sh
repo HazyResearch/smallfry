7fe48df6c6f26285035c859aa40bf878f72dce7c
Avner May <avnermay@cs.stanford.edu>
2018-11-28 00:45:28 -0800

    Another fix to dca_docker.sh

--- src/dca_docker.sh
+++ src/dca_docker.sh
@@ -2,6 +2,6 @@
 
 # Launch docker container, then install git (to get git-hash and git-diff),
 # and launch the command that was passed in as a string to this script.
-cmd="sudo docker run --runtime=nvidia -v /proj:/proj -i --rm tensorflow/tensorflow:latest-gpu-py3 /bin/bash -c \"apt-get --assume-yes update; apt-get --assume-yes install git-core; $1\""
+cmd="sudo docker run --runtime=nvidia -v /proj:/proj --rm tensorflow/tensorflow:latest-gpu-py3 /bin/bash -c \"apt-get --assume-yes update; apt-get --assume-yes install git-core; $1\""
 printf "Execute the following command in docker container: '$cmd'\n"
 eval $cmd
