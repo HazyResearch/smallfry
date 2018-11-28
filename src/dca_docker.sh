0209464be5dfbddf26d4225f1fa66250730bf9f3
avnermay <avnermay@gmail.com>
2018-11-27 19:56:45 -0800

    Adding script to launch DCA training in docker image

---
+++ src/dca_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# NOTE: This only works if called from base conda environment!
+printf "\n**** Updating apt-get and installing git-core. ****\n"
+apt-get --assume-yes update
+apt-get --assume-yes install git-core
+
+# Execute the command that was passed in as a string to this script.
+cmd="sudo docker run --runtime=nvidia -v /proj:/proj -it --rm tensorflow/tensorflow:latest-gpu-py3 $1"
+printf "\nExecute command: '$cmd'\n"
+eval $cmd
