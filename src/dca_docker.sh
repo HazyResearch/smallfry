e7010c9600b437b1fc4187d9f8457a06cc87460a
avnermay <avnermay@gmail.com>
2018-11-27 20:29:54 -0800

    Updates to dca_docker.sh

--- src/dca_docker.sh
+++ src/dca_docker.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# NOTE: This only works if called from base conda environment!
-printf "\n**** Updating apt-get and installing git-core. ****\n"
-apt-get --assume-yes update
-apt-get --assume-yes install git-core
-
-# Execute the command that was passed in as a string to this script.
+# Launch docker container, then install git (to get git-hash and git-diff),
+# and launch the command that was passed in as a string to this script.
 cmd="sudo docker run --runtime=nvidia -v /proj:/proj -it --rm tensorflow/tensorflow:latest-gpu-py3 /bin/bash -c \"apt-get --assume-yes update; apt-get --assume-yes install git-core; $1\""
-printf "\nExecute command: '$cmd'\n"
+printf "\nExecute the following command in docker container: '$cmd'\n"
 eval $cmd
