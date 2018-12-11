9cd0fd4169f1c5dec91ac97abdcfa9f4cb5dc778
Avner May <avnermay@cs.stanford.edu>
2018-12-11 11:30:23 -0800

    Further updates to micro measuring delta1 and delta2 on random data

--- src/experiments.py
+++ src/experiments.py
@@ -29,45 +29,53 @@ def ppmi_spectrum():
     plt.show()
     # display('test')
 
-def deltas_vs_precision_and_lambda():
+def deltas_vs_precision_and_lambda(gaussian):
     n = 1000
     d = 30
     lim = 1.0/np.sqrt(d)
-    X = np.random.uniform(low=-lim, high=lim, size=(n,d))
+    X = (np.random.randn(d,n) * 0.5 * lim if gaussian else
+        np.random.uniform(low=-lim, high=lim, size=(n,d)))
+    X = np.random.randn(d,n) * 0.5 * lim
     K = X @ X.T
     bs = [1,2,4,8,16]
     lambdas = [2**(-32), 2**(-24), 2**(-16), 2**(-12), 2**(-8),2**(-4), 2**(-2), 2**0, 2**1, 2**2, 2**4, 2**8, 2**12, 2**16, 2**20]
-    nplams = np.array(lambdas)
-    x = 2**4 * nplams
     delta1s = np.zeros((len(bs), len(lambdas)))
     delta2s = np.zeros((len(bs), len(lambdas)))
+    # Gather delta1 and delta2 for different precisions and lambdas
     for i_b,b in enumerate(bs):
-        Xq,_,_ = compress.compress_uniform(X, b, adaptive_range=False, stochastic_round=True)
+        Xq,_,_ = compress.compress_uniform(X, b, adaptive_range=gaussian, stochastic_round=True)
         Kq = Xq @ Xq.T
         for i_l,lam in enumerate(lambdas):
             delta1s[i_b,i_l], delta2s[i_b,i_l], _ = utils.delta_approximation(K, Kq, lambda_ = lam)
     plt.figure(1)
     plt.subplot(121)
-    for i_b,b in enumerate(bs):
-        plt.plot(2**b * nplams, delta1s[i_b,:])
-        # plt.plot(2.0**b/((2.0**b-1)**2 * nplams), delta1s[i_b,:])
-        # plt.plot(nplams, delta1s[i_b,:])
-    plt.plot(x, np.sqrt(2*np.log(n/0.1)/d) * 5 * n / x)
-    plt.legend(['1','2','4','8','16','bound'])
-    plt.xscale('log')
-    plt.yscale('log')
+    plot_deltas(delta1s, bs, lambdas, n, d, 'abs(Delta_1)', gaussian)
     plt.subplot(122)
-    for i_b,b in enumerate(bs):
-        plt.plot(2**b * nplams, delta2s[i_b,:])
-        #plt.plot(2.0**b/((2.0**b-1)**2 * nplams), delta2s[i_b,:] - 1.0/((2**b-1)**2 * nplams) )
-        # plt.plot(nplams, delta2s[i_b,:])
-    plt.plot(x, np.sqrt(2*np.log(n/0.1)/d) * 5 * n / x)
-    plt.legend(['1','2','4','8','16','bound'])
-    plt.xscale('log')
-    plt.yscale('log')
+    plot_deltas(delta2s, bs, lambdas, n, d, 'abs(Delta_2)', gaussian)
     plt.show()
     print(1)
 
+def plot_deltas(delta_results, bs, lambdas, n, d, ylabel, gaussian, xlog=True, ylog=True):
+    nplams = np.array(lambdas)
+    x = 2**16 * nplams
+    gaussian_str = 'Gaussian' if gaussian else 'Uniform'
+    for i_b,b in enumerate(bs):
+        if np.alltrue(delta_results[i_b,:] >= 0):
+            print('b={} all positive --- {},{}'.format(b, gaussian_str, ylabel))
+        else:
+            print('b={} contains negative values --- {},{}'.format(b, gaussian_str, ylabel))
+        plt.plot(2**b * nplams, np.abs(delta_results[i_b,:]), marker='o', linestyle='dashed')
+        #plt.plot(2.0**b/((2.0**b-1)**2 * nplams), delta2s[i_b,:] - 1.0/((2**b-1)**2 * nplams) )
+        # plt.plot(nplams, delta2s[i_b,:])
+    plt.plot(x, np.sqrt(2*np.log(n/0.1)/d) * 5 * n / x, marker='o', linestyle='dashed')
+    plt.legend(['1','2','4','8','16','bound'])
+    if xlog: plt.xscale('log')
+    if ylog: plt.yscale('log')
+    plt.xlabel('2^b * lambda')
+    plt.ylabel(ylabel)
+    title = '{}: {} vs. {}'.format(gaussian_str, ylabel, '2^b * lambda')
+    plt.title(title)
+
 if __name__ == '__main__':
     # ppmi_spectrum()
-    deltas_vs_precision_and_lambda()
+    deltas_vs_precision_and_lambda(True)
