991e2d5a3e7b0bd1d561602a1598af0643d12a21
avnermay <avnermay@gmail.com>
2018-12-11 18:23:10 -0800

    Adding story to smallfry notes. Added experiment to understand effect of clipping on D1,D2

--- src/experiments.py
+++ src/experiments.py
@@ -35,7 +35,6 @@ def deltas_vs_precision_and_lambda(gaussian):
     lim = 1.0/np.sqrt(d)
     X = (np.random.randn(d,n) * 0.5 * lim if gaussian else
         np.random.uniform(low=-lim, high=lim, size=(n,d)))
-    X = np.random.randn(d,n) * 0.5 * lim
     K = X @ X.T
     bs = [1,2,4,8,16]
     lambdas = [2**(-32), 2**(-24), 2**(-16), 2**(-12), 2**(-8),2**(-4), 2**(-2), 2**0, 2**1, 2**2, 2**4, 2**8, 2**12, 2**16, 2**20]
@@ -76,6 +75,74 @@ def plot_deltas(delta_results, bs, lambdas, n, d, ylabel, gaussian, xlog=True, y
     title = '{}: {} vs. {}'.format(gaussian_str, ylabel, '2^b * lambda')
     plt.title(title)
 
+def clipping_effect():
+    n = 1000
+    d = 30
+    # lim = 1.0/np.sqrt(d)
+    # X = np.random.randn(d,n) * 0.5 * lim
+    X = np.random.randn(d,n)
+    K = X @ X.T
+    # We are trying to understanding plateau at small 2^b * lam, so take very small lam
+    lam = 2**(-32)
+    max_r = np.max(X)
+    rs = np.linspace(0,max_r,num=20)
+    delta1s = np.zeros(len(rs))
+    delta2s = np.zeros(len(rs))
+    for i_r,r in enumerate(rs):
+        Xq = np.clip(X, -r, r)
+        Kq = Xq @ Xq.T
+        delta1s[i_r], delta2s[i_r], _ = utils.delta_approximation(K, Kq, lambda_ = lam)
+    plt.figure(1)
+    # plt.subplot(121)
+    plt.plot(rs, 1 - delta1s, marker='o', linestyle='dashed', color='b')
+    plt.plot(rs, 1 + delta2s, marker='o', linestyle='dashed', color='b')
+    plt.xlabel('clip value')
+    plt.legend(['1-Delta1','1+Delta2'])
+    plt.title('Effect of clipping on Delta1 and Delta2 for random Gaussian data.')
+    # plt.subplot(122)
+    # plt.plot(rs, delta2s)
+    # plt.xlabel('clip value')
+    # plt.ylabel('Delta_2')
+    plt.show()
+    print(1)
+
+def clipping_with_quantization_effect():
+    n = 1000
+    d = 30
+    X = np.random.randn(d,n)
+    K = X @ X.T
+    bs = [1,2,4,8,32]
+    colors = ['b','g','r','y','k']
+    # We are trying to understanding plateau at small 2^b * lam, so take very small lam
+    lam = 2**(-32)
+    max_r = np.max(X)
+    rs = np.linspace(0,max_r,num=20)
+    delta1s = np.zeros((len(bs),len(rs)))
+    delta2s = np.zeros((len(bs),len(rs)))
+    for i_b,b in enumerate(bs):
+        for i_r,r in enumerate(rs):
+            Xq = compress._compress_uniform(X, b, r, stochastic_round=False)
+            Kq = Xq @ Xq.T
+            delta1s[i_b,i_r], delta2s[i_b,i_r], _ = utils.delta_approximation(K, Kq, lambda_ = lam)
+
+    plt.figure(1)
+    # plt.subplot(121)
+    legend = []
+    for i_b,b in enumerate(bs):
+        plt.plot(rs, 1.0/(1 - delta1s[i_b,:]), marker='o', linestyle='dashed', color=colors[i_b])
+        plt.plot(rs, 1 + delta2s[i_b,:], marker='x', linestyle='dashed', color=colors[i_b])
+        legend.append('1/(1-Delta1) (b={})'.format(b))
+        legend.append('1+Delta2 (b={})'.format(b))
+    plt.xlabel('clip value')
+    plt.legend(legend)
+    plt.title('Effect of clipping + quantization on (Delta1,Delta2) for Gaussian data.')
+    plt.yscale('log')
+    plt.show()
+    print(1)
+
 if __name__ == '__main__':
     # ppmi_spectrum()
-    deltas_vs_precision_and_lambda(True)
+    # deltas_vs_precision_and_lambda(False)
+    # deltas_vs_precision_and_lambda(True)
+    # clipping_effect()
+    clipping_with_quantization_effect()
