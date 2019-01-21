dfd7fa4664fb426baad91c97e0fcd29189e010ac
avnermay <avnermay@gmail.com>
2019-01-21 13:25:01 -0800

    Code to plot intrinsic results

--- src/plotter.py
+++ src/plotter.py
@@ -617,7 +617,7 @@ def plot_embedding_standard_deviation():
 #         for i,embedtype in enumerate(embedtypes):
 #             table_str = 
 
-def plot_metric_vs_performance():
+def plot_metric_vs_performance(y_metric2_evaltype):
     # embedtypes = ['fasttext1m']
     # evaltype = 'synthetics' # this is used in clean results
     
@@ -634,21 +634,27 @@ def plot_metric_vs_performance():
     # GRAM DELTA PLOTS: SYNTHETICS-LARGE-DIM
     embedtypes = ['glove-wiki400k-am','glove400k']
     evaltype = 'synthetics-large-dim'
-    y_metrics = ['gram-large-dim-frob-error', 'subspace-eig-distance', 'subspace-eig-overlap', 'subspace-largest-angle',
+    y_metric1s = ['gram-large-dim-frob-error', 'subspace-eig-distance', 'subspace-eig-overlap', 'subspace-largest-angle',
                  'gram-large-dim-delta1-0', 'gram-large-dim-delta1-1', 'gram-large-dim-delta1-2', 'gram-large-dim-delta1-3', 'gram-large-dim-delta1-4', 'gram-large-dim-delta1-5', 'gram-large-dim-delta1-6',
                  'gram-large-dim-delta1-0-trans', 'gram-large-dim-delta1-1-trans', 'gram-large-dim-delta1-2-trans', 'gram-large-dim-delta1-3-trans', 'gram-large-dim-delta1-4-trans', 'gram-large-dim-delta1-5-trans', 'gram-large-dim-delta1-6-trans']
-    # y_metric2 = 'best-f1'
-    # y_metric2_evaltype = 'qa'
-    dataset = 'trec'
-    y_metric2 = 'test-acc'
-    y_metric2_evaltype = 'sentiment'
+    if y_metric2_evaltype == 'qa':
+        y_metric2s = ['best-f1']
+        dataset = None
+    elif y_metric2_evaltype == 'sentiment':
+        y_metric2s = ['test-acc']
+        dataset = 'trec'
+    elif y_metric2_evaltype == 'intrinsics':
+        y_metric2s = ['analogy-avg-score','similarity-avg-score']
+        dataset = None
+
     logxs = [True,False]
     for embedtype in embedtypes:
-        for y_metric in y_metrics:
-            for logx in logxs:
-                plot_ICML_results(embedtype, evaltype, y_metric, y_metric2=y_metric2,
-                    y_metric2_evaltype=y_metric2_evaltype, scatter=True, logx=logx,
-                    dataset=dataset)
+        for y_metric1 in y_metric1s:
+            for y_metric2 in y_metric2s:
+                for logx in logxs:
+                    plot_ICML_results(embedtype, evaltype, y_metric1, y_metric2=y_metric2,
+                        y_metric2_evaltype=y_metric2_evaltype, scatter=True, logx=logx,
+                        dataset=dataset)
 
 def plot_theorem3_tighter_bound():
     dims = [300,300,200,100,50]
