fa05d3745ea419882e07a5d742f5b7d19e086172
Avner May <avnermay@cs.stanford.edu>
2019-02-03 23:13:36 -0800

    add line to dca_docker.sh which installs nvidia driver 384 as first step

--- src/dca_docker.sh
+++ src/dca_docker.sh
@@ -2,6 +2,7 @@
 
 # Launch docker container, then install git (to get git-hash and git-diff),
 # and launch the command that was passed in as a string to this script.
+sudo apt-get --assume-yes install nvidia-384 nvidia-modprobe
 cmd="sudo docker run --runtime=nvidia -v /proj:/proj --rm tensorflow/tensorflow:latest-gpu-py3 /bin/bash -c \"apt-get --assume-yes update; apt-get --assume-yes install git-core; $1\""
 printf "Execute the following command in docker container: '$cmd'\n"
 eval $cmd
