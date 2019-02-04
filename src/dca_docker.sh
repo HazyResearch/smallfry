b765345362ee4dc3a31b089d267927ae2e448317
Avner May <avnermay@cs.stanford.edu>
2019-02-04 00:27:26 -0800

    Activated smallfry environment in dca_docker.sh

--- src/dca_docker.sh
+++ src/dca_docker.sh
@@ -3,6 +3,6 @@
 # Launch docker container, then install git (to get git-hash and git-diff),
 # and launch the command that was passed in as a string to this script.
 sudo apt-get --assume-yes install nvidia-384 nvidia-modprobe
-cmd="sudo docker run --runtime=nvidia -v /proj:/proj --rm tensorflow/tensorflow:latest-gpu-py3 /bin/bash -c \"apt-get --assume-yes update; apt-get --assume-yes install git-core; $1\""
+cmd="sudo docker run --runtime=nvidia -v /proj:/proj --rm tensorflow/tensorflow:latest-gpu-py3 /bin/bash -c \"apt-get --assume-yes update; apt-get --assume-yes install git-core; . /proj/anaconda3/etc/profile.d/conda.sh; conda activate smallfry; conda env list; which python; $1\""
 printf "Execute the following command in docker container: '$cmd'\n"
 eval $cmd
