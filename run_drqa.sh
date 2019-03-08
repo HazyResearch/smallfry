2104a29cfb3abb109e1686701a21ace86c64bc02
Jian Zhang <zhangjianthu@gmail.com>
2019-03-07 22:42:40 -0800

    sync release repo

--- run_drqa.sh
+++ run_drqa.sh
@@ -2,4 +2,4 @@
 
 # python -u ./src/smallfry/third_party/DrQA/scripts/reader/train.py --embed-dir= --embedding-file ./src/glove.6B.300d.txt --embedding-dim 300 --random-seed 1
 
-python -u ./src/smallfry/third_party/DrQA/scripts/reader/train.py --embed-dir='' --embedding-file ./src/smallfry/third_party/DrQA/data/glove.6B.300d.txt --embedding-dim 300 --random-seed 1 --use-quant-embed --nbit 4
+python -u ./src/smallfry/third_party/DrQA/scripts/reader/train.py --embed-dir='' --embedding-file ./src/smallfry/third_party/DrQA/data/glove.6B.300d.txt --embedding-dim 300 --random-seed 1 --use-quant-embed --nbit 32 
