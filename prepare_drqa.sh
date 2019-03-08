3b0e64e95828b54647eda104019c997d146450ca
Jian Zhang <zhangjianthu@gmail.com>
2019-03-07 18:29:12 -0800

    update prepare drqa script

--- prepare_drqa.sh
+++ prepare_drqa.sh
@@ -2,4 +2,12 @@
 
 pushd ./src/smallfry/third_party/DrQA
 pip install -r requirements.txt; python setup.py develop
+mkdir data
+popd
+
+pushd ./src/smallfry/third_party/DrQA/data
+wget https://www.dropbox.com/s/tdekiyr8r14ogtk/datasets.zip?dl=0
+unzip dtasets.zip
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip
 popd
