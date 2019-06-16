f8b08866a51fd2fa29a416a1a85870c1b06032fc
zjian <zjian@stanford.edu>
2019-06-15 18:17:40 -0700

    update the drqa run scripts

--- prepare_drqa.sh
+++ prepare_drqa.sh
@@ -7,7 +7,7 @@ popd
 
 pushd ./src/smallfry/third_party/DrQA/data
 wget https://www.dropbox.com/s/tdekiyr8r14ogtk/datasets.zip?dl=0
-unzip dtasets.zip
+unzip datasets.zip
 wget http://nlp.stanford.edu/data/glove.6B.zip
 unzip glove.6B.zip
 popd
